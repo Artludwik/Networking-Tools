6842ed03cfa0b8af8b53981578c380a4b027a6ee
www.linkedin.com/in/artur-ulaszek <arthurhurricane5@gmail.com>
2025-06-22 20:23:37 +0200

    Add files via upload
    3 basic command collected in a one shortscript to ensure how your internet connection is working.

---
+++ network_checker.bat
@@ -0,0 +1,11 @@
+@echo off
+color 70
+echo Narzedzie do sprawdzania polaczenia internetowego.
+echo Autor: Artur Ulaszek, mail:rossmann@kozacki.pl
+echo.
+ping 127.0.0.1
+TIMEOUT /T 3 > NUL
+pathping google.com
+TIMEOUT /T 3 > NUL
+nslookup google.com
+pause
